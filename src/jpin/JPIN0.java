d1712b74006a1d343e2fff81ef75baad075ed21e
室谷優 <murotaniyu@Mac.local>
2021-03-17 19:33:30 +0900

    First commit

---
+++ src/jpin/JPIN0.java
@@ -0,0 +1,10 @@
+package jpin;
+
+public class JPIN0 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("JIPN0");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author murotaniyu
+ *
+ */
+module JPIN {
+}
